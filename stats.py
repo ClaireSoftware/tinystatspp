e364f787355eb78e8b61a265f26417cef5c4a203
ClaireSoftware <gameboy193@gmail.com>
2018-10-08 00:46:21 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Copyright (c) 2017 Adafruit Industries
-# Author: Tony DiCola & James DeVito
+# Author: Tony DiCola & James DeVito & Claire Chambers
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
